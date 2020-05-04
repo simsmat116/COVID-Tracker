8e1d4267388274fcf015cd26f75aab43072c3e5e
simsmat116 <simsmat11697@gmail.com>
2020-05-04 17:49:09 -0400

    Setup a task to retrieve case data from the API by specifying a date range in the enpoint. Next step is to setup a task to call the summary endpoint to get an overview of all the data that gets updated daily.

--- dags/covid_post_dag.py
+++ dags/covid_post_dag.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import datetime, date
 from airflow import DAG
 from airflow.hooks.http_hook import HttpHook
 from airflow.exceptions import AirflowException
@@ -21,7 +21,6 @@ def retrieve_countries():
     http = HttpHook(method='GET', http_conn_id="covid_api")
     # Send request to the countries endpoint
     resp = http.run("countries")
-    print(resp)
 
     # Raise exception to cause task to fail if API provides bad response
     if http.check_response(resp):
@@ -34,17 +33,40 @@ def retrieve_countries():
     while index < len(countries):
         # Generate the current file number for the block
         file_num = index // block_size
-        # Overwriting any existing data and placing new csv header
-        with open("../staging/country" + str(file_num) + ".csv", "w") as file:
-            file.write("country, slug\n")
+
+        # Erase contents of the existing file
+        open("../staging/country" + str(file_num) + ".txt", "w").close()
 
         # Placing the appropriate date into the file
-        with open("../staging/country" + str(file_num) + ".csv", "a") as file:
+        with open("../staging/country" + str(file_num) + ".txt", "a") as file:
             # Iterate the current block of countries
             for country in countries[index:min(index+block_size, len(countries))]:
-                file.write(country["Country"] + ", " + country["Slug"] + "\n")
+                file.write(country["Slug"] + "\n")
 
         index = index+block_size
 
+def country_cases(**kwargs):
+    # Setup a hook to the API endpoint
+    http = HttpHook(method="GET", http_conn_id="covid_api")
 
-retrieve_countries()
+    with open("../staging/country" + kwargs["file_num"] + ".txt") as file:
+        for line in file:
+            country = line.rstrip("\n")
+
+            # Read the latest date that
+            date_file = open("../staging/last_date.txt")
+            from_date = date_file.readline()
+            date_file.close()
+            to_date = date.today().strftime("%Y-%m-%d") + "T00:00:00Z"
+            print(to_date)
+            # Create the endpoint that specifies range of dates
+            endpoint = "country/" + country + "?from=" + from_date + "&to=" + to_date
+            resp = http.run(endpoint)
+
+            for case in resp.json():
+                print(case)
+
+
+
+#retrieve_countries()
+country_cases(file_num="0")
